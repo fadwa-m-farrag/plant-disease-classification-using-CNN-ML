1e350fb76747985306a03e72497cc9657a0f4131
fadwa-m-farrag <fadwa.mmfami@gmail.com>
2025-06-20 03:24:10 +0300

    updated the shallow CNN + ML model

--- cnn.py
+++ cnn.py
@@ -1,33 +1,102 @@
 import torch
 import torch.nn as nn
-import torch.nn.functional as F 
 from torch.utils.data import Dataset, Subset, DataLoader 
-import torch.optim as optim
-from tqdm import tqdm
-from torchvision import datasets, transforms
+from torchvision import datasets
+from torchvision.transforms import v2 
 from sklearn.model_selection import train_test_split 
+from sklearn.decomposition import PCA 
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score, classification_report
 import numpy as np 
 
 device = torch.device('cuda')
 
-# splitting the folders into train & test datasets without messing up
-# the folder structure of my dataset 
-transform = transforms.Compose([
-    transforms.Resize((128, 128)),
-    transforms.ToTensor
+train_transform = v2.Compose([
+    v2.Resize((128, 128)),
+    #v2.Grayscale(num_output_channels=1),
+    v2.RandomHorizontalFlip(p=0.2),
+    v2.ColorJitter(brightness=0.1, contrast=0.1),
+    v2.ToImage(),
+    v2.ToDtype(torch.float32, scale=True)
     ])
 
-dataset = datasets.ImageFolder(root='/home/fadwa/Projects/Diploma Project Data/Data for Identification of Plant Leaf Diseases Using a 9-layer Deep Convolutional Neural Network', transform=transform) 
+test_transform = v2.Compose([
+     v2.Resize((128,128)),
+     v2.ToImage(),
+     v2.ToDtype(torch.float32, scale=True)
+])
 
-targets = np.array(dataset.targets)
+path = '/home/fadwa/Projects/Diploma Project Data/Data for Identification of Plant Leaf Diseases Using a 9-layer Deep Convolutional Neural Network/Plant_leave_diseases_dataset_without_augmentation/Apple'
+        
+full_train_dataset = datasets.ImageFolder(root=path, transform=train_transform) 
+full_test_dataset = datasets.ImageFolder(root=path, transform=test_transform)
+
+targets = np.array(full_train_dataset.targets)
 indices = np.arange(len(targets))
+class_names = full_train_dataset.classes
 
 train_idx, test_idx = train_test_split(indices, test_size=0.2, stratify=targets, random_state = 42)
 
-train_dataset = Subset(dataset, train_idx)
-test_dataset = Subset(dataset, test_idx)
+train_dataset = Subset(full_train_dataset, train_idx)
+test_dataset = Subset(full_test_dataset, test_idx)
 
-train_loader = DataLoader(train_dataset, batch_size=32, shuffle=True)
-test_loader = DataLoader(test_dataset, batch_size=32, shuffle=False)
+train_loader = DataLoader(train_dataset, batch_size=8, shuffle=True)
+test_loader = DataLoader(test_dataset, batch_size=8, shuffle=False)
 
+class CNN(nn.Module):
+    def __init__(self):
+        super(CNN, self).__init__()
+        self.conv_layer = nn.Sequential(
+                nn.Conv2d(3, 16, kernel_size=2, padding=1),
+                nn.ReLU(),
+                nn.Conv2d(16, 16, kernel_size=2, padding=1),
+                nn.ReLU(),
+                nn.MaxPool2d(2),
+
+                nn.Conv2d(16, 32, kernel_size=2, padding=1),
+                nn.ReLU(),
+                nn.Conv2d(32, 32, kernel_size=2, padding=1),
+                nn.ReLU(),
+                nn.MaxPool2d(2),
+
+                nn.Conv2d(32, 64, kernel_size=2, padding=1),
+                nn.ReLU(),
+                nn.Conv2d(64, 64, kernel_size=2, padding=1),
+                nn.ReLU(),
+                nn.AdaptiveAvgPool2d((1,1))
+                )
+        
+    def forward(self, x):
+            x = self.conv_layer(x)
+            return x.view(x.size(0), -1)
+
+model = CNN().to(device)
+model.eval()
+
+def feature_extraction(dataloader):
+    features = []
+    labels = []
+    with torch.no_grad():
+        for images, lbls in dataloader:
+            images = images.to(device)
+            feats = model(images)
+            features.append(feats.cpu().numpy())
+            labels.extend(lbls.numpy())
+
+    return np.vstack(features), np.array(labels)
+
+x_train, y_train = feature_extraction(train_loader)
+x_test, y_test = feature_extraction(test_loader)
+
+pca = PCA(n_components=0.99)
+x_train_pca = pca.fit_transform(x_train)
+x_test_pca = pca.transform(x_test)
+
+knn = KNeighborsClassifier(n_neighbors=5)
+knn.fit(x_train_pca, y_train)
+
+y_pred = knn.predict(x_test_pca)
+print("Accuracy:", accuracy_score(y_test, y_pred))
+print(classification_report(y_test,y_pred, target_names = class_names))
 
